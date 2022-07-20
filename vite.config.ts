7d1e2fe2d2e1835346803ab5cd63f692997509b6
auzmartist <auz.martin@gmail.com>
2022-07-20 11:13:30 -0700

    Release 0.3.4
    Changelog:
    - remove umd named export
    - redefine builds around ESM .js and CommonJS .umd.cjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     emptyOutDir: false,
     lib: {
-      entry: "./src/index.ts",
-      name: "CssHMR",
-      formats: ["es", "umd", "cjs"],
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "CssHmr",
+      fileName: "rollup-plugin-css-hmr",
     },
     rollupOptions: {
       external: ["path"],
+      output: {
+        globals: {
+          path: "path",
+        },
+      },
     },
   },
 });
